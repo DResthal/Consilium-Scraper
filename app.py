2f5c216eaf2f91430dc06c99dbab0a973aaae363
DralrinResthal <c.neilclack@gmail.com>
2021-01-16 12:27:15 -0600

    Setup project

---
+++ app.py
@@ -0,0 +1,6 @@
+import os
+import selenium
+import pandas as pd
+
+# Get all items, prices and number of reviews on first 5 pages.
+target_url = 'https://webscraper.io/test-sites/e-commerce/ajax/computers/laptops'
