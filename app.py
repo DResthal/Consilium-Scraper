a01f1d258f4b3249e71d7f0bd5742146806642a0
DralrinResthal <c.neilclack@gmail.com>
2021-01-17 16:05:38 -0600

    Get all items

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import sys
 import time
 import json
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import pandas as pd
@@ -20,7 +22,6 @@ webdriver_path = os.path.abspath("chromedriver_win32\chromedriver.exe")
 
 driver = webdriver.Chrome(webdriver_path)
 
-
 try:
     driver.get(target_url)
     logging.info(f"Successfully connected to {target_url}")
@@ -29,14 +30,75 @@ except:
     logging.warning(f"Error connecting to {target_url}")
     logging.warning(e)
 
+try:
+    if driver.find_element_by_id("closeCookieBanner").is_displayed():
+        driver.find_element_by_id("closeCookieBanner").click()
+        logging.info("Closed Cookie Banner")
+except:
+    e = sys.exc_info()
+    logging.warning("Faile to close cookie banner")
+    logging.warning(e)
 
-list_of_items = driver.find_element_by_class_name("ecomerce-items").get_attribute(
-    "data-items"
-)
+
+def get_page_items():
+    try:
+        item_list = driver.find_elements_by_css_selector("div.thumbnail")
+        logging.debug(f"Found item_list")
+        return item_list
+    except:
+        e = sys.exc_info()
+        logging.warning("Failed to find item_list")
+        logging.warning(e)
+        pass
 
 
-json_of_items = json.loads(list_of_items)
+action = ActionChains(driver)
 
-logging.info(json.dumps(json_of_items, sort_keys=True, indent=2))
+end_item_list = []
+
+for i in range(5):
+    i += 1
+    logging.info("Running get_page_items()")
+    print(i)
+    item_list = get_page_items()
+    for item in item_list:
+        try:
+            item_dict = {
+                "title": item.find_element_by_class_name("title").text,
+                "price": item.find_element_by_class_name("price").text,
+                "description": item.find_element_by_class_name("description").text,
+                "reviews": item.find_element_by_class_name("ratings").text,
+                "url": item.find_element_by_class_name("title").get_attribute("href"),
+            }
+            end_item_list.append(item_dict)
+            logging.info(item_dict)
+        except:
+            e = sys.exc_info()
+            print(e)
+            pass
+
+    x_path = f"/html/body/div[1]/div[3]/div/div[2]/div[2]/button[{i}]"
+    print(f"xpath: {x_path}")
+    logging.info("Looking for pagination button")
+
+    if i != 1:
+        try:
+            button = driver.find_element_by_xpath(x_path)
+            logging.info("Found pagination button")
+            button.click()
+            logging.info("Pagination button has been clicked")
+        except:
+            e = sys.exc_info()
+            logging.warning("Failed to find or click pagination button")
+            logging.warning(e)
+    else:
+        print(f"Skipping {i}")
+        pass
+
+    time.sleep(1)
+
+df = pd.DataFrame(end_item_list)
+df = df.drop_duplicates()
+df.to_csv("test.csv")
 
 driver.quit()
